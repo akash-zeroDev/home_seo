9aa4f7b4d020421997eb81b1e217a93f6c0e54f0
droidDev-Ak <programcoder.ak@gmail.com>
2026-05-17 21:29:58 +0530

    feat: add @next/third-parties package

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// Import the optimized Next.js Google Analytics component
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,7 +45,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        {/* Your specific Google Analytics ID is injected here */}
+        <GoogleAnalytics gaId="G-G3XBTRSS98" />
+      </body>
     </html>
   );
 }
